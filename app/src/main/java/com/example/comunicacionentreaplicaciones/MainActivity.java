dd847152913abccbfebb58ee2805c1a3c4b76f88
pinkiwinkie <elrumor99@gmail.com>
2023-04-19 21:18:34 +0200

    v.3

--- app/src/main/java/com/example/comunicacionentreaplicaciones/MainActivity.java
+++ app/src/main/java/com/example/comunicacionentreaplicaciones/MainActivity.java
@@ -1,33 +1,56 @@
 package com.example.comunicacionentreaplicaciones;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 private Button bVerificar;
+private TextView tvResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bVerificar = findViewById(R.id.verificar);
+        tvResult = findViewById(R.id.tvResultAccept);
+
+
+
+        ActivityResultLauncher<Intent> someActivityResultLauncher =
+                registerForActivityResult(
+                        new ActivityResultContracts.StartActivityForResult(),
+                        result -> {
+// No es necesario identificar la actividad que envia resultados
+                            if(result.getResultCode() == RESULT_CANCELED)
+                                Toast.makeText(this, "Cancelado por el usuario",
+                                        Toast.LENGTH_LONG).show();
+                            else if (result.getResultCode() == Activity.RESULT_OK) {
+                                Intent data = result.getData();
+                                String usuario = data.getExtras().getString("resultado");
+                                tvResult.setText(usuario);
+                            }
+                        });
+
         bVerificar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                lanzarVerificar(null);
+                Intent i = new Intent(this, Verificar.class);
+                EditText editText = findViewById(R.id.editTextTextPersonName);
+                i.putExtra("name",editText.getText().toString());
+                someActivityResultLauncher.launch(i);
             }
         });
     }
 
-    private void lanzarVerificar(View view) {
-        Intent i = new Intent(this, Verificar.class);
-        EditText editText = findViewById(R.id.editTextTextPersonName);
-        i.putExtra("name",editText.getText().toString());
-        startActivity(i);
+
     }
-}
+
--- app/src/main/java/com/example/comunicacionentreaplicaciones/Verificar.java
+++ app/src/main/java/com/example/comunicacionentreaplicaciones/Verificar.java
@@ -1,18 +1,39 @@
 package com.example.comunicacionentreaplicaciones;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Verificar extends AppCompatActivity {
+
+    private Button bAccept, bCancel;
     @Override
     public  void onCreate(Bundle savedInstace){
         super.onCreate(savedInstace);
+        bAccept = findViewById(R.id.aceptar);
+        bCancel = findViewById(R.id.rechazar);
         setContentView(R.layout.verificar);
         TextView textView = findViewById(R.id.textViewWithInformation);
         Bundle extras = getIntent().getExtras();
         String name = extras.getString("name");
         textView.setText("Hola " + name+", ¿Aceptas las condiciones?");
+
+        bAccept.setOnClickListener(view -> {
+            Intent intent = new Intent();
+            setResult(RESULT_OK, intent);
+            intent.putExtra("resultado", "Condiciones aceptadas");
+            finish();
+        });
+
+        bCancel.setOnClickListener(view -> {
+            Intent intent = new Intent();
+            setResult(RESULT_CANCELED, intent);
+            finish();
+        });
     }
 }
